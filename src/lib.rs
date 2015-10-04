299cc2f7a7de192867772aa187df938e92113c14
Tibor Benke <ihrwein@gmail.com>
2015-10-04 10:31:37 +0200

    lib: define MatcherSuite trait
    This trait can be used to create suites which includes
    how to insantiate the parsers and the matcher. By
    implementing this trait you can use the generic benchmarks
    and tests.
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-#[test]
-fn it_works() {
+#![feature(plugin,const_fn)]
+#![plugin(stainless)]
+extern crate actiondb;
+extern crate regex;
+
+use actiondb::Matcher;
+use actiondb::matcher::MatcherFactory;
+use actiondb::parsers::{
+    ParserFactory
+};
+
+pub trait MatcherSuite {
+    type Matcher: Matcher;
+    type ParserFactory: ParserFactory;
+    type MatcherFactory: MatcherFactory<Matcher=Self::Matcher>;
+
+    fn parser_factory() -> Self::ParserFactory;
+    fn matcher_factory() -> Self::MatcherFactory;
 }
