131e47cf7ad7c95ff2c25f9537ea4bd901349c28
Tibor Benke <ihrwein@gmail.com>
2015-10-04 10:37:26 +0200

    benches: bench RegexMatcherSuite as well
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- benches/lib.rs
+++ benches/lib.rs
@@ -20,10 +20,19 @@ fn create_matcher<T>() -> T::Matcher
 }
 
 use matcher_benchmarks::suites::TrieMatcherSuite;
+use matcher_benchmarks::suites::RegexMatcherSuite;
 
 #[bench]
 fn bench_trie_matcher_with_ssh_logs(b: &mut Bencher) {
     let matcher = create_matcher::<TrieMatcherSuite>();
+    b.iter(|| {
+        matcher.parse("Jun 25 14:09:41 lobotomy sshd[26478]: pam_unix(sshd:session): session closed for user zts");
+    });
+}
+
+#[bench]
+fn bench_regex_matcher_with_ssh_logs(b: &mut Bencher) {
+    let matcher = create_matcher::<RegexMatcherSuite>();
     b.iter(|| {
         matcher.parse("Jun 25 14:09:41 lobotomy sshd[26478]: pam_unix(sshd:session): session closed for user zts");
     });
