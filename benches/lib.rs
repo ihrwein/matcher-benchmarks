55b42d036d71e02118d39f10ccb72e1be08b9655
Tibor Benke <ihrwein@gmail.com>
2015-10-07 06:59:32 +0200

    benches: add some more benchmarks
    and temporarily disable suffix arrays.
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- benches/lib.rs
+++ benches/lib.rs
@@ -11,7 +11,8 @@ use test::Bencher;
 
 fn create_matcher<T>() -> T::Matcher
     where T: MatcherSuite {
-    let path = "benches/ssh_ok.json";
+    //let path = "benches/ssh_ok.json";
+    let path = "/home/tibi/Documents/Diplomamunka/samples/default.json";
     let result = GenericFactory::from_json_file::<T::MatcherFactory>(path);
     if result.is_err() {
         println!("{:?}", &result);
@@ -22,18 +23,23 @@ fn create_matcher<T>() -> T::Matcher
 use matcher_benchmarks::suites::TrieMatcherSuite;
 use matcher_benchmarks::suites::RegexMatcherSuite;
 
-#[bench]
-fn bench_trie_matcher_with_ssh_logs(b: &mut Bencher) {
-    let matcher = create_matcher::<TrieMatcherSuite>();
-    b.iter(|| {
-        matcher.parse("Jun 25 14:09:41 lobotomy sshd[26478]: pam_unix(sshd:session): session closed for user zts");
-    });
+
+macro_rules! bench {
+    ($test_name:ident, $test_message:expr, $suite:ty) => {
+        #[bench]
+        fn $test_name(b: &mut Bencher) {
+            let matcher = create_matcher::<$suite>();
+            b.iter(|| {
+                matcher.parse($test_message);
+            });
+        }
+    }
 }
 
-#[bench]
-fn bench_regex_matcher_with_ssh_logs(b: &mut Bencher) {
-    let matcher = create_matcher::<RegexMatcherSuite>();
-    b.iter(|| {
-        matcher.parse("Jun 25 14:09:41 lobotomy sshd[26478]: pam_unix(sshd:session): session closed for user zts");
-    });
-}
+const TEST_MESSAGE_WHICH_MATCHES: &'static str = "martian source 10.0.106.201 from 10.0.69.167, on dev eth0";
+bench!(bench_trie_matcher_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite);
+bench!(bench_regex_matcher_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite);
+
+const TEST_MESSAGE_WHICH_DOES_NOT_MATCH: &'static str = r#"type=1400 audit(1444191210.403:63): apparmor="STATUS" operation="profile_replace" profile="unconfined" name="docker-default" pid=2782 comm="apparmor_parser"#;
+bench!(bench_trie_matcher_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuite);
+bench!(bench_regex_matcher_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, RegexMatcherSuite);
