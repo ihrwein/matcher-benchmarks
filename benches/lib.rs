01003fde031c64b9a79b4d1c7ce4d2b97cdb6031
Tibor Benke <ihrwein@gmail.com>
2015-11-02 18:28:12 +0100

    src: move test related constants to lib.rs
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- benches/lib.rs
+++ benches/lib.rs
@@ -4,32 +4,24 @@ extern crate matcher_benchmarks;
 extern crate actiondb;
 
 use actiondb::matcher::GenericFactory;
-use actiondb::matcher::Matcher;
 use actiondb::matcher::MatcherFactory;
 use actiondb::matcher::MatcherSuite;
-use test::Bencher;
 use actiondb::parsers::ParserFactory;
 
-fn create_matcher<T>() -> T::Matcher
-    where T: MatcherSuite {
-    //let path = "benches/ssh_ok.json";
-    let path = "/home/tibi/Documents/Diplomamunka/samples/default.json";
-    let result = GenericFactory::from_json_file::<T::MatcherFactory>(path);
-    if result.is_err() {
-        println!("{:?}", &result);
-    }
-    result.ok().expect("Failed to create a Matcher object")
-}
-
-use actiondb::matcher::trie::TrieMatcherSuite;
-use matcher_benchmarks::suites::RegexMatcherSuite;
-use matcher_benchmarks::suites::SuffixArrayMatcherSuite;
+pub use actiondb::matcher::Matcher;
+pub use test::Bencher;
+pub use actiondb::matcher::trie::TrieMatcherSuite;
+pub use matcher_benchmarks::suites::RegexMatcherSuite;
+pub use matcher_benchmarks::suites::SuffixArrayMatcherSuite;
+pub use matcher_benchmarks::create_matcher;
+pub use matcher_benchmarks::TrieMatcherSuiteWithRegexParsers;
 
 macro_rules! bench {
-    ($test_name:ident, $test_message:expr, $suite:ty) => {
+    ($test_name:ident, $test_message:expr, $suite:ty, $path:expr) => {
+        #[allow(non_snake_case)]
         #[bench]
         fn $test_name(b: &mut Bencher) {
-            let matcher = create_matcher::<$suite>();
+            let matcher = create_matcher::<$suite>($path);
             b.iter(|| {
                 matcher.parse($test_message);
             });
@@ -37,22 +29,88 @@ macro_rules! bench {
     }
 }
 
-const TEST_MESSAGE_WHICH_MATCHES: &'static str = "martian source 10.0.106.201 from 10.0.69.167, on dev eth0";
-bench!(bench_trie_matcher_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite);
-bench!(bench_trie_matcher_with_regex_parsers_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuiteWithRegexParsers);
-bench!(bench_regex_matcher_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite);
-bench!(bench_suffix_array_matcher_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, SuffixArrayMatcherSuite);
 
-const TEST_MESSAGE_WHICH_DOES_NOT_MATCH: &'static str = r#"type=1400 audit(1444191210.403:63): apparmor="STATUS" operation="profile_replace" profile="unconfined" name="docker-default" pid=2782 comm="apparmor_parser"#;
-bench!(bench_trie_matcher_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuite);
-bench!(bench_trie_matcher_with_regex_parsers_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuiteWithRegexParsers);
-bench!(bench_regex_matcher_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, RegexMatcherSuite);
-bench!(bench_suffix_array_matcher_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, SuffixArrayMatcherSuite);
+mod test_500_patterns {
+    use super::*;
+    use matcher_benchmarks::test_datas::pattern_500::*;
 
-struct TrieMatcherSuiteWithRegexParsers;
+    bench!(bench_TrieMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuite, PATTERN_FILE);
 
-impl MatcherSuite for TrieMatcherSuiteWithRegexParsers {
-    type Matcher = <TrieMatcherSuite as MatcherSuite>::Matcher;
-    type ParserFactory = <RegexMatcherSuite as MatcherSuite>::ParserFactory;
-    type MatcherFactory = <TrieMatcherSuite as MatcherSuite>::MatcherFactory;
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuiteWithRegexParsers, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuiteWithRegexParsers,PATTERN_FILE);
+
+    bench!(bench_RegexMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite, PATTERN_FILE);
+    bench!(bench_RegexMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, RegexMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_SuffixArrayMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, SuffixArrayMatcherSuite, PATTERN_FILE);
+    bench!(bench_SuffixArrayMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, SuffixArrayMatcherSuite, PATTERN_FILE);
+}
+
+mod test_400_patterns {
+    use super::*;
+    use matcher_benchmarks::test_datas::pattern_400::*;
+
+    bench!(bench_TrieMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuiteWithRegexParsers, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuiteWithRegexParsers,PATTERN_FILE);
+
+    bench!(bench_RegexMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite, PATTERN_FILE);
+    bench!(bench_RegexMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, RegexMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_SuffixArrayMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, SuffixArrayMatcherSuite, PATTERN_FILE);
+    bench!(bench_SuffixArrayMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, SuffixArrayMatcherSuite, PATTERN_FILE);
+}
+
+mod test_300_patterns {
+    use super::*;
+    use matcher_benchmarks::test_datas::pattern_300::*;
+
+    bench!(bench_TrieMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuiteWithRegexParsers, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuiteWithRegexParsers,PATTERN_FILE);
+
+    bench!(bench_RegexMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite, PATTERN_FILE);
+    bench!(bench_RegexMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, RegexMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_SuffixArrayMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, SuffixArrayMatcherSuite, PATTERN_FILE);
+    bench!(bench_SuffixArrayMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, SuffixArrayMatcherSuite, PATTERN_FILE);
+}
+
+mod test_200_patterns {
+    use super::*;
+    use matcher_benchmarks::test_datas::pattern_200::*;
+
+    bench!(bench_TrieMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuiteWithRegexParsers, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuiteWithRegexParsers,PATTERN_FILE);
+
+    bench!(bench_RegexMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite, PATTERN_FILE);
+    bench!(bench_RegexMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, RegexMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_SuffixArrayMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, SuffixArrayMatcherSuite, PATTERN_FILE);
+    bench!(bench_SuffixArrayMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, SuffixArrayMatcherSuite, PATTERN_FILE);
+}
+
+mod test_100_patterns {
+    use super::*;
+    use matcher_benchmarks::test_datas::pattern_100::*;
+
+    bench!(bench_TrieMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuiteWithRegexParsers, PATTERN_FILE);
+    bench!(bench_TrieMatcherSuiteWithRegexParsers_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, TrieMatcherSuiteWithRegexParsers,PATTERN_FILE);
+
+    bench!(bench_RegexMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite, PATTERN_FILE);
+    bench!(bench_RegexMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, RegexMatcherSuite, PATTERN_FILE);
+
+    bench!(bench_SuffixArrayMatcherSuite_when_message_matches, TEST_MESSAGE_WHICH_MATCHES, SuffixArrayMatcherSuite, PATTERN_FILE);
+    bench!(bench_SuffixArrayMatcherSuite_when_message_does_not_match, TEST_MESSAGE_WHICH_DOES_NOT_MATCH, SuffixArrayMatcherSuite, PATTERN_FILE);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,97 @@ extern crate actiondb;
 extern crate regex;
 #[macro_use]
 extern crate maplit;
+#[macro_use]
+extern crate log;
 
 pub mod suites;
+
+use actiondb::matcher::GenericFactory;
+use actiondb::matcher::MatcherFactory;
+use actiondb::matcher::MatcherSuite;
+use actiondb::matcher::Matcher;
+use actiondb::matcher::trie::TrieMatcherSuite;
+use suites::RegexMatcherSuite;
+use suites::SuffixArrayMatcherSuite;
+
+pub fn create_matcher<T>(path: &str) -> T::Matcher
+    where T: MatcherSuite {
+    let result = GenericFactory::from_json_file::<T::MatcherFactory>(path);
+    if result.is_err() {
+        println!("{:?}", &result);
+    }
+    result.ok().expect("Failed to create a Matcher object")
+}
+
+pub struct TrieMatcherSuiteWithRegexParsers;
+
+impl MatcherSuite for TrieMatcherSuiteWithRegexParsers {
+    type Matcher = <TrieMatcherSuite as MatcherSuite>::Matcher;
+    type ParserFactory = <RegexMatcherSuite as MatcherSuite>::ParserFactory;
+    type MatcherFactory = <TrieMatcherSuite as MatcherSuite>::MatcherFactory;
+}
+
+pub mod test_datas {
+    pub mod pattern_500 {
+        pub const EXPECTED_UUID: &'static str = "e05d0227-5cae-45c9-96bd-121aef46f9b8";
+        pub const TEST_MESSAGE_WHICH_MATCHES: &'static str = "statistics: max cache size 1 at Nov 9 15:02:42";
+        pub const TEST_MESSAGE_WHICH_DOES_NOT_MATCH: &'static str = r#"[origin software="rsyslogd" swVersion="7.4.4" x-pid="665" x-info="http://www.rsyslog.com"] start"#;
+        pub const PATTERN_FILE: &'static str = "/home/tibi/Documents/Diplomamunka/samples/default_500.json";
+    }
+    pub mod pattern_400 {
+        pub const TEST_MESSAGE_WHICH_MATCHES: &'static str = "(ResourceName) Failed to call refresh: /usr/bin/nova-manage db sync returned 1 instead of one of [0] at /var/tmp/packstack/cd468d5155d64ab1b31d9dfed7c6b847/modules/nova/manifests/api.pp:100";
+        pub const TEST_MESSAGE_WHICH_DOES_NOT_MATCH: &'static str = r#"[origin software="rsyslogd" swVersion="7.4.4" x-pid="665" x-info="http://www.rsyslog.com"] start"#;
+        pub const PATTERN_FILE: &'static str = "/home/tibi/Documents/Diplomamunka/samples/default_400.json";
+    }
+    pub mod pattern_300 {
+        pub const TEST_MESSAGE_WHICH_MATCHES: &'static str = "lost connection after CONNECT from unknown[192.168.0.2]";
+        pub const TEST_MESSAGE_WHICH_DOES_NOT_MATCH: &'static str = r#"[origin software="rsyslogd" swVersion="7.4.4" x-pid="665" x-info="http://www.rsyslog.com"] start"#;
+        pub const PATTERN_FILE: &'static str = "/home/tibi/Documents/Diplomamunka/samples/default_300.json";
+    }
+    pub mod pattern_200 {
+        pub const TEST_MESSAGE_WHICH_MATCHES: &'static str = r#"plugin_dispatch_values: Dataset not found: ps_state (from "docker/plugin-inst/ps_state-blocked"), check your types.db!"#;
+        pub const TEST_MESSAGE_WHICH_DOES_NOT_MATCH: &'static str = r#"[origin software="rsyslogd" swVersion="7.4.4" x-pid="665" x-info="http://www.rsyslog.com"] start"#;
+        pub const PATTERN_FILE: &'static str = "/home/tibi/Documents/Diplomamunka/samples/default_200.json";
+    }
+    pub mod pattern_100 {
+        pub const TEST_MESSAGE_WHICH_MATCHES: &'static str = "Warning: cgroup_attach_task_pid failed: 5236";
+        pub const TEST_MESSAGE_WHICH_DOES_NOT_MATCH: &'static str = r#"[origin software="rsyslogd" swVersion="7.4.4" x-pid="665" x-info="http://www.rsyslog.com"] start"#;
+        pub const PATTERN_FILE: &'static str = "/home/tibi/Documents/Diplomamunka/samples/default_100.json";
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    pub use actiondb::matcher::result::MatchResult;
+    pub use actiondb::matcher::Matcher;
+    pub use actiondb::matcher::trie::suite::TrieMatcherSuite;
+    pub use suites::RegexMatcherSuite;
+    pub use suites::SuffixArrayMatcherSuite;
+    pub use ::TrieMatcherSuiteWithRegexParsers;
+
+macro_rules! test_matching {
+    ($test_name:ident, $test_message:expr, $suite:ty, $path:expr, $uuid:expr) => {
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        #[test]
+        fn $test_name() {
+            let matcher = create_matcher::<$suite>($path);
+            let result: Option<MatchResult> = matcher.parse($test_message);
+            let got_uuid: String = result.unwrap().pattern().uuid().to_hyphenated_string();
+            assert_eq!(&got_uuid, $uuid);
+        }
+    }
+}
+
+    #[cfg(test)]
+    mod pattern_500 {
+        use ::create_matcher;
+        use super::*;
+
+        use test_datas::pattern_500::*;
+        test_matching!(TrieMatcherSuite_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuite, PATTERN_FILE, EXPECTED_UUID);
+        test_matching!(TrieMatcherSuiteWithRegexParsers_matches, TEST_MESSAGE_WHICH_MATCHES, TrieMatcherSuiteWithRegexParsers, PATTERN_FILE, EXPECTED_UUID);
+        test_matching!(SuffixArrayMatcherSuite_matches, TEST_MESSAGE_WHICH_MATCHES, SuffixArrayMatcherSuite, PATTERN_FILE, EXPECTED_UUID);
+        test_matching!(RegexMatcherSuite_matches, TEST_MESSAGE_WHICH_MATCHES, RegexMatcherSuite, PATTERN_FILE, EXPECTED_UUID);
+    }
+}
