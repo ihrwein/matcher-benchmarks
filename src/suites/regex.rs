69d5e99d198dcf895cecf4da78f2f0d070b646cd
Tibor Benke <ihrwein@gmail.com>
2015-11-02 18:30:04 +0100

    regex: add min_len/max_len support and fix a bug (remaining_len)
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/suites/regex.rs
+++ src/suites/regex.rs
@@ -19,10 +19,21 @@ pub mod parsers {
     #[derive(Debug, Clone)]
     pub struct SetParser {
         name: Option<String>,
-        regex: Regex
+        regex: Regex,
+        min_len: Option<usize>,
+        max_len: Option<usize>,
     }
 
-    macro_rules! regex_parser_body {
+    impl SetParser {
+        pub fn set_min_length(&mut self, value: usize) {
+            self.min_len = Some(value);
+        }
+        pub fn set_max_length(&mut self, value: usize) {
+            self.max_len = Some(value);
+        }
+    }
+
+    macro_rules! regex_parser_parse {
         () => {
             fn parse<'a, 'b>(&'a self, value: &'b str) -> Option<ParseResult<'a, 'b>> {
                 if let Some((_, end)) = self.regex.find(value) {
@@ -31,6 +42,11 @@ pub mod parsers {
                     None
                 }
             }
+        }
+    }
+
+    macro_rules! regex_parser_name {
+        () => {
             fn name(&self) -> Option<&str> {
                 self.name.as_ref().map(|name| name.borrow())
             }
@@ -41,12 +57,35 @@ pub mod parsers {
     }
 
     impl Parser for SetParser {
-        regex_parser_body!();
+        regex_parser_name!();
+
+        fn parse<'a, 'b>(&'a self, value: &'b str) -> Option<ParseResult<'a, 'b>> {
+            if let Some((_, end)) = self.regex.find(value) {
+                let parsed_value = &value[..end];
+                if let Some(min_len) = self.min_len {
+                    if parsed_value.len() < min_len {
+                        return None;
+                    }
+                }
+
+                if let Some(max_len) = self.max_len {
+                    if parsed_value.len() > max_len {
+                        return None;
+                    }
+                }
+
+                Some(ParseResult::new(self, &value[..end]))
+            } else {
+                None
+            }
+        }
 
         fn boxed_clone(&self) -> Box<Parser> {
             let parser = SetParser {
                 name: self.name.clone(),
-                regex: self.regex.clone()
+                regex: self.regex.clone(),
+                min_len: self.min_len.clone(),
+                max_len: self.max_len.clone()
             };
             Box::new(parser)
         }
@@ -94,15 +133,46 @@ pub mod parsers {
     #[derive(Debug)]
     pub struct GreedyParser {
         name: Option<String>,
+        end_string: Option<String>,
         regex: Regex
     }
 
+    impl GreedyParser {
+        pub fn new(name: Option<&str>, end_string: Option<&str>) -> GreedyParser {
+            let regex = match end_string {
+                Some(end_string) => {
+                    Regex::new(&format!("{}", end_string)).ok().expect("Failed to create a GreedyParser")
+                },
+                None => {
+                    Regex::new(".*").ok().expect("Failed to create a GreedyParser")
+                }
+            };
+            GreedyParser {
+                name: name.map(|name| name.to_string()),
+                end_string: end_string.map(|end| end.to_string()),
+                regex: regex
+            }
+        }
+    }
+
     impl Parser for GreedyParser {
-        regex_parser_body!();
+        regex_parser_name!();
+
+        fn parse<'a, 'b>(&'a self, value: &'b str) -> Option<ParseResult<'a, 'b>> {
+            if self.end_string.is_none() {
+                return Some(ParseResult::new(self, value));
+            }
+            if let Some((_, end)) = self.regex.find(value) {
+                Some(ParseResult::new(self, &value[..end]))
+            } else {
+                None
+            }
+        }
 
         fn boxed_clone(&self) -> Box<Parser> {
             let parser = GreedyParser {
                 name: self.name.clone(),
+                end_string: self.end_string.clone(),
                 regex: self.regex.clone()
             };
             Box::new(parser)
@@ -119,39 +189,62 @@ pub mod parsers {
 
     pub struct RegexParserFactory;
 
+    macro_rules! set_optinal_param {
+        ($parser:expr, $param:expr) => {
+            match $param {
+                OptionalParameter::Int(key, value) => {
+                    match key {
+                        "min_len" => {
+                            $parser.set_min_length(value);
+                        },
+                        "max_len" => {
+                            $parser.set_max_length(value);
+                        },
+                        _ => ()
+                    }
+                }
+            }
+        }
+    }
+
+    macro_rules! set_optional_params {
+        ($parser:expr, $opt_params:expr) => {
+            if let Some(opt_params) = $opt_params {
+                for i in opt_params.into_iter() {
+                    set_optinal_param!($parser, i);
+                }
+            }
+        }
+    }
+
     impl ParserFactory for RegexParserFactory {
-        fn new_set<'a>(set: &str, name: Option<&str>, _: Option<Vec<OptionalParameter<'a>>>) -> Box<Parser> {
-            let regex = Regex::new(&format!("^[{}]", set)).ok().expect("Failed to create a SetParser");
-            let parser = SetParser {
+        fn new_set<'a>(set: &str, name: Option<&str>, opt_params: Option<Vec<OptionalParameter<'a>>>) -> Box<Parser> {
+            let regex = Regex::new(&format!("^[{}]+", set)).ok().expect("Failed to create a SetParser");
+            let mut parser = SetParser {
                 name: name.map(|name| name.to_string()),
-                regex: regex
+                regex: regex,
+                min_len: None,
+                max_len: None
             };
 
+            set_optional_params!(&mut parser, opt_params);
             Box::new(parser)
         }
-        fn new_int<'a>(name: Option<&str>, _: Option<Vec<OptionalParameter<'a>>>) -> Box<Parser> {
-            let regex = Regex::new(&format!("^[0123456789]")).ok().expect("Failed to create an IntParser");
-            let parser = IntParser {
+        fn new_int<'a>(name: Option<&str>, opt_params: Option<Vec<OptionalParameter<'a>>>) -> Box<Parser> {
+            let regex = Regex::new(&format!("^[0123456789]+")).ok().expect("Failed to create an IntParser");
+            let mut parser = IntParser {
                 delegate: SetParser {
                     name: name.map(|name| name.to_string()),
-                    regex: regex
+                    regex: regex,
+                    min_len: None,
+                    max_len: None
                 }
             };
+            set_optional_params!(&mut parser.delegate, opt_params);
             Box::new(parser)
         }
         fn new_greedy<'a>(name: Option<&str>, end_string: Option<&str>) -> Box<Parser> {
-            let regex = match end_string {
-                Some(end_string) => {
-                    Regex::new(&format!("{}", end_string)).ok().expect("Failed to create a GreedyParser")
-                },
-                None => {
-                    Regex::new(&format!("$")).ok().expect("Failed to create a GreedyParser")
-                }
-            };
-            let parser = GreedyParser {
-                name: name.map(|name| name.to_string()),
-                regex: regex
-            };
+            let parser = GreedyParser::new(name, end_string);
             Box::new(parser)
         }
     }
@@ -171,8 +264,8 @@ pub mod matcher {
 
     impl Matcher for RegexMatcher {
         fn parse<'a, 'b>(&'a self, text: &'b str) -> Option<MatchResult<'a, 'b>> {
-            let mut remaining_text = text;
             for pattern in &self.patterns {
+                let mut remaining_text = text;
                 let mut match_result = MatchResult::new(pattern);
                 for token in pattern.pattern() {
                     match *token {
